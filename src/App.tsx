b294da27657707b74d55cbfb2bdd041949d5d29f
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-07-31 20:37:44 -0300

    setup layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,53 @@
 import Home from "./pages/home/Home.tsx";
 import Users from "./pages/users/Users.tsx";
 import Products from "./pages/products/Products.tsx";
+import Login from "./pages/login/Login.tsx";
+import Navbar from "./components/navbar/Navbar.tsx";
+import Menu from "./components/menu/Menu.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-const Layout = () => {};
+const Layout = () => {
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+    ],
   },
   {
-    path: "users",
-    element: <Users />,
-  },
-  {
-    path: "products",
-    element: <Products />,
+    path: "login",
+    element: <Login />,
   },
 ]);
 
